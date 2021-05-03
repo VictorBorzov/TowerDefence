92ed402168bf12251587176ef44d7c6a91919c5f
borzov <borzov.vk@gmail.com>
2021-05-03 08:28:09 +0300

    Seminar 2. Grid.

---
+++ Assets/Scripts/Field/Grid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Field
+{
+    public class Grid
+    {
+        private Node[,] _nodes;
+
+        private int _width;
+        
+        private int _height;
+
+        public Grid(int width, int height)
+        {
+            _width = width;
+            _height = height;
+
+            _nodes = new Node[width, height];
+            for (int i = 0; i < _nodes.GetLength(0); ++i)
+            for (int j = 0; j < _nodes.GetLength(1); ++j)
+                 _nodes[i, j] = new Node();
+        }
+
+        public Node GetNode(Vector2Int coordinate)
+        {
+            return GetNode(coordinate.x, coordinate.y);
+        }
+        
+        public Node GetNode(int i, int j)
+        {
+            if (i < 0 || i >= _width)
+                return null;
+            if (j < 0 || j >= _height)
+                return null;
+            return _nodes[i, j];
+        }
+    }
+}
---
+++ Assets/Scripts/Field/GridHolder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace Field
+{
+    public class GridHolder : MonoBehaviour
+    {
+        [SerializeField] 
+        private int gridWidth;
+
+        [SerializeField] 
+        private int gridHeight;
+
+        [SerializeField]
+        private float nodeSize;
+        
+        private Grid _grid;
+
+        private Camera _camera;
+
+        private Vector3 _offset;
+        private void Awake()
+        {
+            _grid = new Grid(gridWidth, gridHeight);
+            _camera = Camera.main;
+
+            var width = gridWidth * nodeSize;
+            var height = gridHeight * nodeSize;
+            
+            // Default plane size is 10 by 10
+            transform.localScale = new Vector3(width * 0.1f, 1f, height * 0.1f);
+
+            _offset = transform.position - (new Vector3(width, 0f, height) * 0.5f);
+        }
+
+        private void Update()
+        {
+            if (_grid == null || _camera == null)
+                return;
+            
+            Vector3 mousePosition = Input.mousePosition;
+
+            Ray ray = _camera.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform != transform)
+                    return;
+                
+                Vector3 hitPosition = hit.point;
+                Vector3 difference = hitPosition - _offset;
+
+                int x = (int) (difference.x / nodeSize);
+                int z = (int) (difference.z / nodeSize);
+                Debug.Log($"{x} {z}");
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(_offset, 0.1f);
+        }
+    }
+}
