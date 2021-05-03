ea7adc4811c636c59a520635f6e6d573d015aeb8
borzov <borzov.vk@gmail.com>
2021-05-03 06:39:04 +0300

    Seminar 1. Simple script for object movement.

---
+++ Assets/Scripts/MovementAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovementAgent : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed;
+
+    [SerializeField] 
+    private Vector3 target;
+    
+    private const float Tolerance = 0.1f;
+    
+    void Update()
+    {
+        float distance = (target - transform.position).magnitude; 
+        if (distance < Tolerance)
+            return;
+        Vector3 direction = (target - transform.position).normalized;
+        Vector3 delta = direction * (speed * Time.deltaTime );
+        transform.Translate(delta);
+    }
+}
